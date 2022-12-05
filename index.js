8bdeca053080e6f90f7e467256b08a99aef25afa
James <jjguevarag@gmail.com>
2022-12-05 15:02:35 -0600

    .

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 // Ejercicios:
 // 1. Llene una lista con los numeros del 1 al 1000, lo puede hacer con cualquier metodo visto er
 // clase: 10 puntos
+function lista1000(){
+  let lista = [];
+  for(let i = 1; i <= 1000; i++){
+    lista.push(i);
+  }
+  return lista;
+}
 
+console.log(lista1000());
 
 // 2. Utilizando la siguiente let Lista = [2,3,4,5,6,7,8,10], realice las siguientes operaciones:
 // a. Elimine el primero (5 puntos)
@@ -9,8 +17,37 @@
 // C. Sume todos los elementos (5 puntos)
 // d. Muestre unicamente los que sean pares (5 puntos)
 
+let Lista = [2,3,4,5,6,7,8,10];
+Lista.shift();
+
+Lista.pop();
+
+let suma = 0;
+for (let i = 0; i < Lista.length; i++) {
+  suma += Lista[i];
+}
+
+let pares = [];
+for (let i = 0; i < Lista.length; i++) {
+  if (Lista[i] % 2 === 0) {
+    pares.push(Lista[i]);
+  }
+}
+
+console.log(pares);
+
 // 3. Llene una matriz con numeros impares, la matriz debe de ser 3 x3, o sea 3 filas, 3 columnas: 10 puntos
 
+let matriz = [[], [], []];
+let numero = 1;
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+        // @ts-ignore
+        matriz[i][j] = numero;
+        numero += 2;
+    }
+}
+console.log(matriz);
 
 //4. Realice una calculadora utilzando funciones, debe realizar las siguientes operaciones
 // matematicas:
@@ -21,6 +58,38 @@
 // e. Raiz cuadrada (5 puntos)
 // f. Potencia (5 puntos)
 
+// Función para suma
+function sumar(num1, num2) {
+  return num1 + num2;
+}
+
+// Función para restar
+function resta(num1, num2) {
+  return num1 - num2;
+}
+
+// Función para dividir
+function dividir(num1, num2) {
+  if (num2 == 0) {
+    return "No se puede dividir entre 0";
+  }
+  return num1 / num2;
+}
+
+// Función para multiplicar
+function multiplicar(num1, num2) {
+  return num1 * num2;
+}
+
+// Función para sacar raíz cuadrada
+function raizCuadrada(num) {
+  return Math.sqrt(num);
+}
+
+// Función para sacar potencia
+function potencia(num1, num2) {
+  return Math.pow(num1, num2);
+}
 
 
 // Note: Los representan amigos. Suponga que los indices representan personas y que
